3b74ae64efa899df1f483030bfe1296bb78f6293
ollav12 <90145593+ollav12@users.noreply.github.com>
2024-10-08 11:30:15 +0200

    Finished part 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,8 @@ dependencies {
     implementation(platform("org.hibernate.orm:hibernate-platform:6.6.0.Final"))
     implementation("org.hibernate.orm:hibernate-core")
     implementation("jakarta.transaction:jakarta.transaction-api")
-    implementation("com.h2database:h2:2.2.220")
+    implementation("org.postgresql:postgresql:42.7.4")
+
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
